ec84716274819e3e4e85b036398520cdd6ceebff
Marco Longobardi <mrclngbrd@gmail.com>
2021-10-12 18:24:35 +0200

    Update mongoDB.config.js

--- mongoDB.config.js
+++ mongoDB.config.js
@@ -1,4 +1,4 @@
 var mongoose = require('mongoose')
-var mongoDB = 'mongodb+srv://m220student:m220student@mflix.pnhbt.mongodb.net/test?retryWrites=true&w=majority'
+var mongoDB = ''
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true});
 module.exports = mongoose
